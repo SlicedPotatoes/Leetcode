c67c5fc6869a07db03643a87b5036724c60b4ea6
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2024-02-20 20:47:50 +0100

    1431 Commentaire

--- 1431. Kids With the Greatest Number of Candies/main.cpp
+++ 1431. Kids With the Greatest Number of Candies/main.cpp
@@ -3,19 +3,21 @@ class Solution
   public:
     vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
     {
-        int biggest = 0;
-        vector<bool> result(candies.size());
+        int biggest = 0; // Variable pour stocker le plus grand nombre de bonbons initialisée à zéro
+        vector<bool> result(candies.size()); // Vecteur pour stocker le résultat pour chaque enfant
 
+        // Chercher le plus grand nombre de bonbons
         for (int c : candies)
         {
-            biggest = max(c, biggest);
+            biggest = max(c, biggest); // Met à jour le plus grand nombre de bonbons si un nombre plus grand est trouvé
         }
 
+        // Calculer le résultat pour chaque enfant
         for (int indexKids = 0; indexKids < candies.size(); indexKids++)
         {
             if (candies[indexKids] + extraCandies >= biggest)
             {
-                result[indexKids] = true;
+                result[indexKids] = true; // Si un enfant peut avoir le plus grand nombre de bonbons, marquer comme vrai
             }
         }
 
