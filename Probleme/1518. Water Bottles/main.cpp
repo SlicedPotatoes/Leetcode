bd2b3ed57f93d344e433f43a1cc872d4fff2fcf4
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2024-07-07 19:46:12 +0200

    Update main.cpp

--- Probleme/1518. Water Bottles/main.cpp
+++ Probleme/1518. Water Bottles/main.cpp
@@ -3,17 +3,18 @@ class Solution
   public:
     int numWaterBottles(int numBottles, int numExchange)
     {
-        int ans = 0;
+        int ans = 0; // Total des bouteilles bues
 
+        // Tant qu'il y a assez de bouteilles pour les échanger contre de nouvelles
         while (numBottles >= numExchange)
         {
-            int r = numBottles % numExchange;
-            ans += numBottles - r;
+            int r = numBottles % numExchange; // Bouteilles restantes après échange (pleine)
+            ans += numBottles - r;            // Ajouter les bouteilles bues
 
-            numBottles /= numExchange;
-            numBottles += r;
+            numBottles /= numExchange; // Mettre à jour le nombre de bouteilles après échange
+            numBottles += r;           // Ajouter les bouteilles restantes pour le prochain échange
         }
 
-        return ans + numBottles;
+        return ans + numBottles; // Retourner le nombre total de bouteilles bues
     }
 };
